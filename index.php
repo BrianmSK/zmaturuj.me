f45b91db47d7b8c156c0b5d23f573bd47ee0e141
Denis Uhrík <uhrikdenis@gmail.com>
2021-12-09 13:24:26 +0100

    Adding stylesheets, contact form and footer

--- index.php
+++ index.php
@@ -5,22 +5,31 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="_partils/main.css">
+  <link rel="stylesheet" href="_partials/css/normalize.css?ver=<?php echo filemtime("_partials/css/normalize.css") ?>">
+  <link rel="stylesheet" href="_partials/css/main.css?ver=<?php echo filemtime("_partials/css/main.css") ?>">
   <title>Domov</title>
 </head>
 
 <body>
-
-  <header class="navigation">
-
-  </header>
-
   <main class="website-content">
+    <h1>Prihlásenie</h1>
+    <form action="login_form.php" method="POST">
+      <div class="container">
+        <input type="text" placeholder="Zadajte vase prihlasovacie meno" required class="nameInput">
 
+        <input type="password" placeholder="Zadajte heslo" required class="passwordInput">
+
+        <button type="submit">Prihlásenie</button>
+
+        <label>
+          <input type="checkbox" checked="checked" name="remember"> Zapamätajte si ma
+        </label>
+      </div>
+    </form>
   </main>
 
   <footer class="footer">
-
+    <h2>Made with <3 by Denis and Samuel</h2>
   </footer>
 
 </body>
