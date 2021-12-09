a45c765d36f1cb764395ae9accccd20933b62640
Denis Uhrík <uhrikdenis@gmail.com>
2021-12-09 12:09:41 +0100

    Clean Commit

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="_partils/main.css">
   <title>Domov</title>
 </head>
 
 <body>
 
-  <header class="navigation"> asdasffjsksksksk
-    Samo je frajer
+  <header class="navigation">
+
   </header>
 
   <main class="website-content">
