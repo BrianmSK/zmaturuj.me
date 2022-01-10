389459110341d9700bf6571be72c4af895c3cd4c
Denis Uhrík <uhrikdenis@gmail.com>
2022-01-10 02:38:38 +0100

    Added composer, phpmailer and flash messages

--- public/index.php
+++ public/index.php
@@ -1,37 +1,18 @@
-<!DOCTYPE html>
-<html lang="sk-SK">
+<?php
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="_partials/css/normalize.css?ver=<?php echo filemtime("_partials/css/normalize.css") ?>">
-  <link rel="stylesheet" href="_partials/css/main.css?ver=<?php echo filemtime("_partials/css/main.css") ?>">
-  <title>Domov</title>
-</head>
+require_once("../config/config.php");
 
-<body>
-  <main class="website-content">
-    <h1>Prihlásenie</h1>
-    <form action="login_form.php" method="POST">
-      <input type="text" placeholder="Zadajte vaše prihlasovacie meno" required class="nameInput">
+if ($path == "/") {
+  include_once("../views/components/site.php");
+  die();
+}
 
-      <input type="password" placeholder="Zadajte heslo" required class="passwordInput">
-
-      <button type="submit" class="buttonInput">Prihlásenie</button>
-
-      <label class="checkboxContainer">
-        <input type="checkbox" checked="checked" name="remember" class="checkboxInput">
-        Zapamätajte si ma
-      </label>
-
-    </form>
-  </main>
-
-  <footer class="footer">
-    <h2>Made with <3 by Denis and Samuel</h2>
-  </footer>
-
-</body>
-
-</html>
+if ($path == "/generate") {
+  include_once("../views/frontend/generate.html");
+  die();
+}
+if ($path == "/error") {
+  header("HTTP/1.1 404 Not Found");
+  include_once("../views/error/404.html");
+  die();
+}
