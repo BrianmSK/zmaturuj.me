797478b89f143064c77a32afbfce578d713c6a20
Denis Uhrík <uhrikdenis@gmail.com>
2022-01-23 20:27:42 +0100

    added login checking, logout and erroring

--- public/index.php
+++ public/index.php
@@ -6,16 +6,25 @@ require_once("../classes/is_logged.php");
 if ($path == "/" && is_logged()) {
   include_once("../views/frontend/welcome.php");
   die();
-} else {
+} elseif ($path == "/" && !is_logged()) {
   include_once("../views/frontend/login.php");
+  die();
+} elseif (strpos($_SERVER['REQUEST_URI'], "?") && is_logged() && isset($_GET['logout'])) {
+  if ($_GET['logout'] == true) {
+    session_destroy();
+    header("Location: /zmaturuj.me/");
+    die();
+  }
   die();
 }
 
 if ($path == "/generate") {
   include_once("../views/frontend/generate.php");
   die();
-}
-if ($path == "/error") {
+} elseif ($path == "/error") {
+  include_once("../views/error/404.php");
+  die();
+} else {
   include_once("../views/error/404.php");
   die();
 }
