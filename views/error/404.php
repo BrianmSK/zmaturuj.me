06e043f115733b80d7f1719186ac14dcf80bb8b1
Denis Uhrík <uhrikdenis@gmail.com>
2022-01-24 00:09:06 +0100

    adding CSP support and favicon

--- views/error/404.php
+++ views/error/404.php
@@ -8,6 +8,8 @@ header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src 'self' cdn.jsdelivr.net; connect-src 'self'; img-src 'self'; style-src 'self' cdn.jsdelivr.net fonts.googleapis.com;base-uri 'self';form-action 'self'; font-src 'self' fonts.gstatic.com">
+  <link rel="shortcut icon" href="/zmaturuj.me/public/media/favicon.png" type="image/png">
   <title>404 Ooopsie</title>
   <meta http-equiv="refresh" content="5;url=/zmaturuj.me/" />
 </head>
