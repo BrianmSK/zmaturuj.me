4f0b42821475c01f1a37e14b5d1d360fa9a6f752
Denis Uhrík <uhrikdenis@gmail.com>
2022-01-24 19:17:02 +0100

    clean welcome page, routing on index file

--- views/frontend/welcome.php
+++ views/frontend/welcome.php
@@ -1,40 +1,19 @@
 <?php
 include_once "../config/config.php";
-include_once "../classes/reset.php";
 
 $title = "Zmaturuj.me | Welcome";
 
 include_once "parts/header.php";
 
-if (isReset($connection, $_SESSION['id'])) {
-
 ?>
-  <main class="website-content">
-    <?= $msg->display(); ?>
-    <h1>Please enter new password!</h1>
-    <form action="../zmaturuj.me/views/auth/reset_password.php" method="post">
-
-      <input type="password" name="password" placeholder="Zadajte heslo" required class="passwordInput">
-
-      <input type="password" name="password-verify" placeholder="Zopakujte heslo" required class="passwordInput">
-
-      <button type="submit" name="submit" class="buttonInput">Change</button>
-
-    </form>
-  </main>
+<main class="website-content">
+  <?= $msg->display(); ?>
+  <h1>Welcome</h1>
+  <h2>
+    <a href="/zmaturuj.me/?logout=true">Log out</a>
+  </h2>
+</main>
 
 <?php
-
-} else {
-?>
-  <main class="website-content">
-    <?= $msg->display(); ?>
-    <h1>Welcome</h1>
-    <h2>
-      <a href="/zmaturuj.me/?logout=true">Log out</a>
-    </h2>
-  </main>
-<?php
-}
 include_once "parts/footer.php";
 ?>
