412f4540d4b5503044af313c5421b19037ca16c3
Denis Uhrík <uhrikdenis@gmail.com>
2022-01-24 19:19:03 +0100

    On form, reset proper data in database

---
+++ views/auth/reset_password.php
@@ -0,0 +1,35 @@
+<?php
+include_once("../../config/config.php"); // INCLUDE CONFIG
+
+if (isset($_POST['submit'])) {
+  if ($_POST['password'] != $_POST['password-verify']) {
+    $msg->error('Password does not match! Enter again', "http://localhost/zmaturuj.me/?reset-password=$reset_hash", true);
+  } else {
+    $password = $_POST['password'];
+    $reset_hash = $_POST['hash'];
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    if (isset($_SESSION['id'])) {
+      $password_change = $connection->prepare("
+      UPDATE users
+      SET password = :password, reset = 0, reset_hash = NULL
+      WHERE id = :id
+    ");
+      $password_change->bindParam(':password', $hashed_password);
+      $password_change->bindParam(':id', $_SESSION['id']);
+      $password_change->execute();
+      session_destroy();
+      $msg->success('Password resetted via id!', 'http://localhost/zmaturuj.me/', true);
+    } else {
+      $password_change = $connection->prepare("
+      UPDATE users
+      SET password = :password, reset = 0, reset_hash = NULL
+      WHERE reset_hash = :reset
+    ");
+      $password_change->bindParam(':password', $hashed_password);
+      $password_change->bindParam(':reset', $reset_hash);
+      $password_change->execute();
+      session_destroy();
+      $msg->success('Password resetted via hash ver!', 'http://localhost/zmaturuj.me/', true);
+    }
+  }
+}
