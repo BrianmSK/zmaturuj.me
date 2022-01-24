ec9e19d5fde031f4cdb0688720d26d4ceca44a18
Denis Uhrík <uhrikdenis@gmail.com>
2022-01-24 19:18:06 +0100

    edited routing and added conditions

--- public/index.php
+++ public/index.php
@@ -3,21 +3,28 @@
 require_once("../config/config.php");
 require_once("../classes/is_logged.php");
 require_once("../classes/is_admin.php");
+require_once("../classes/is_reset.php");
 
 /*
 CHECK IF USER IS LOGGED
 */
-if ($path == "/" && is_logged()) {
-  include_once("../views/frontend/welcome.php");
+if ($path == "/" && is_logged() && isReset($connection, $_SESSION['id'])) {
+  include_once("../views/frontend/reset.php");
   die();
   /*
 CHECK IF USER IS GOING TO RESET PASSWORD VIA LINK
 */
-} elseif (strpos($_SERVER['REQUEST_URI'], "?") && is_logged() && isset($_GET['reset-password'])) {
+} elseif (strpos($_SERVER['REQUEST_URI'], "?") && !is_logged() && isset($_GET['reset-password'])) {
+  include_once("../views/frontend/reset.php");
+  die();
+  /*
+CHECK IF LOGGED AND RESET?
+*/
+} elseif ($path == "/" && is_logged()) {
   include_once("../views/frontend/welcome.php");
   die();
   /*
-CHECK IF IS NOT LOGGED
+CHECK IF LOGGED
 */
 } elseif ($path == "/" && !is_logged()) {
   include_once("../views/frontend/login.php");
