65a3ec0a723366890b23ea8330ffd6bb8fc18ae6
Denis Uhrík <uhrikdenis@gmail.com>
2022-01-24 16:57:32 +0100

    lightened code and added important stuff to routing

--- public/index.php
+++ public/index.php
@@ -2,13 +2,29 @@
 
 require_once("../config/config.php");
 require_once("../classes/is_logged.php");
+require_once("../classes/is_admin.php");
 
+/*
+CHECK IF USER IS LOGGED
+*/
 if ($path == "/" && is_logged()) {
   include_once("../views/frontend/welcome.php");
   die();
+  /*
+CHECK IF USER IS GOING TO RESET PASSWORD VIA LINK
+*/
+} elseif (strpos($_SERVER['REQUEST_URI'], "?") && is_logged() && isset($_GET['reset-password'])) {
+  include_once("../views/frontend/welcome.php");
+  die();
+  /*
+CHECK IF IS NOT LOGGED
+*/
 } elseif ($path == "/" && !is_logged()) {
   include_once("../views/frontend/login.php");
   die();
+  /*
+CHECK IF USER WANTS TO LOGOUT
+*/
 } elseif (strpos($_SERVER['REQUEST_URI'], "?") && is_logged() && isset($_GET['logout'])) {
   if ($_GET['logout'] == true) {
     session_destroy();
@@ -18,13 +34,17 @@ if ($path == "/" && is_logged()) {
   die();
 }
 
-if ($path == "/generate") {
+/*
+CHECK IF USER WANTS TO GENERATE ACCOUNT AND IS LOGGED AND IS ADMIN
+*/
+if ($path == "/generate" && is_logged() && is_admin($connection, $_SESSION['id'])) {
   include_once("../views/frontend/generate.php");
   die();
 } elseif ($path == "/error") {
   include_once("../views/error/404.php");
   die();
 } else {
+  header("Location: /zmaturuj.me/error");
   include_once("../views/error/404.php");
   die();
 }
