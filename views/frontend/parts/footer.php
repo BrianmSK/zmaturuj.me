fa7b89101c96c61e44120e3bcc636f2fbf86dca4
Denis Uhrík <uhrikdenis@gmail.com>
2022-01-24 16:57:57 +0100

    added header & footer file

---
+++ views/frontend/parts/footer.php
@@ -0,0 +1,7 @@
+ <footer class="footer">
+   <h2>Made with <3 by Denis and Samuel</h2>
+ </footer>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+ </body>
+
+ </html>
