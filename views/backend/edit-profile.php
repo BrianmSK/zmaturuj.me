58b836e8e93295d81e02bb355bd38e9c0eca87fa
Denis Uhrík <uhrikdenis@gmail.com>
2022-02-06 22:32:53 +0100

    Added possibility of editing profile data via form when user is logged in

---
+++ views/backend/edit-profile.php
@@ -0,0 +1,79 @@
+<?php
+
+// INCLUDE CONFIG
+include_once '../config/config.php';
+if (isset($_POST['submit'])) {
+
+  // LOWER AND SANITIZE MAIL INPUT
+  $email = strtolower(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL));
+  $firstname = htmlspecialchars($_POST['firstname']);
+  $lastname = htmlspecialchars($_POST['lastname']);
+  $password = $_POST['passwd'];
+  $confirm_password = $_POST['confirm_passwd'];
+
+  $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+  // CHECK IS PASSWORD IS EMPTY, IF ITS NOT, UPDATE WITH PASSWORD
+  if (!empty($password)) {
+
+    // IS PASSWORDS DOESNT EQUAL, RETURN WITH ERROR
+    if ($password != $confirm_password) {
+      $msg->error('Password does not match! Enter again', "$url/profile", true);
+      die();
+    }
+
+    // CHECK IF EMAIL EITHER EXISTS OR IS NOT OWNED BY USER
+    if (email_exists($connection, $email) == false) {
+
+      // PREPARE QUERY TO UPDATE WITH NEW PASSWORD
+      $send_variables = $connection->prepare("
+        UPDATE users
+        SET firstname = :firstname, lastname = :lastname, email = :email, password = :password
+        WHERE id = :id
+      ");
+      $send_variables->bindParam(":firstname", $firstname);
+      $send_variables->bindParam(":lastname", $lastname);
+      $send_variables->bindParam(":email", $email);
+      $send_variables->bindParam(":password", $password_hash);
+      $send_variables->bindParam(":id", $_SESSION['id']);
+      $send_variables->execute();
+
+      // REDIRECT TO HOMEPAGE WITH SUCCESS
+      $msg->success('Profile updated!', $url, true);
+      die();
+
+      // OTHERWISE REDIRECT TO PROFILE WITH ERROR
+    } else {
+      $msg->error('Email already exists!', "$url/profile", true);
+      die();
+    }
+  }
+  // IF PASSWORD IS EMPTY UPDATE WITHOUT PASSWORD
+  else {
+
+    // CHECK IF EMAIL EITHER EXISTS OR IS NOT OWNED BY USER
+    if (email_exists($connection, $email) == false) {
+
+      // PREPARE QUERY TO UPDATE WITHOUT PASSWORD
+      $send_variables = $connection->prepare("
+        UPDATE users
+        SET firstname = :firstname, lastname = :lastname, email = :email
+        WHERE id = :id
+      ");
+      $send_variables->bindParam(":firstname", $firstname);
+      $send_variables->bindParam(":lastname", $lastname);
+      $send_variables->bindParam(":email", $email);
+      $send_variables->bindParam(":id", $_SESSION['id']);
+      $send_variables->execute();
+
+      // REDIRECT TO HOMEPAGE WITH SUCCESS
+      $msg->success('Profile updated!', $url, true);
+      die();
+
+      // OTHERWISE REDIRECT TO PROFILE WITH ERROR
+    } else {
+      $msg->error('Email already exists!', "$url/profile", true);
+      die();
+    }
+  }
+}
---
+++ views/frontend/profile.php
@@ -0,0 +1,39 @@
+<?php
+
+if (is_logged()) {
+
+  $title = "Zmaturuj.me | User profile";
+
+  include_once "parts/header.php";
+
+  // QUERY TO GET VALUES FOR FORM
+  $profile_values = $connection->prepare("
+  SELECT firstname, lastname, email
+  FROM users
+  WHERE id = :id
+  ");
+  $profile_values->bindParam(":id", $_SESSION['id']);
+  $profile_values->execute();
+  $profile_values_result = $profile_values->fetch(PDO::FETCH_ASSOC);
+?>
+  <header>
+    <a href="<?= $url ?>">Go back</a>
+  </header>
+  <main class="website-content">
+    <?= $msg->display(); ?>
+    <form action="/zmaturuj.me/edit-profile" method="post">
+      <input type="text" required name="firstname" placeholder="Meno" class="input" value="<?= $profile_values_result['firstname'] ?>">
+      <input type="text" required name="lastname" placeholder="Priezvisko" class="input" value="<?= $profile_values_result['lastname'] ?>">
+      <input type="email" required name="email" placeholder="E-mail" class="input" value="<?= $profile_values_result['email'] ?>">
+      <input type="password" name="passwd" placeholder="Heslo" class="input">
+      <input type="password" name="confirm_passwd" placeholder="Potvrdte heslo" class="input">
+      <button type="submit" name="submit" class="buttonInput">Odoslať</button>
+    </form>
+  </main>
+
+<?php
+  include_once "parts/footer.php";
+} else {
+  header("Location: $url");
+}
+?>
