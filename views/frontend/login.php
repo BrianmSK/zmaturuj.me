594eccf035c70acadeae63701dd1ef9f883f0b6a
BrianmSK <grafikbrianm@gmail.com>
2022-02-14 08:14:31 +0100

    Changed login page to included header to fix menu that shouldnt be showed...

--- views/frontend/login.php
+++ views/frontend/login.php
@@ -1,25 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="sk-SK">
 
-// INCLUDE TITLE FOR HEADER
-$title = "Zmaturuj.me | Login page";
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="/zmaturuj.me/public/media/favicon.png" type="image/png">
+  <link rel="stylesheet" type="text/css" href="/zmaturuj.me/public/css/normalize.css?ver=<?php echo filemtime("../public/css/normalize.css") ?>">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="/zmaturuj.me/public/css/main.css?ver=<?php echo filemtime("../public/css/main.css") ?>">
+  <title>Zmaturuj.me | Login page</title>
 
-// INCLUDE HEADER
-include_once "parts/header.php"; ?>
+</head>
 
-<main class="website-content">
-  <?php $msg->display(); ?>
-  <h1>Prihlásenie</h1>
-  <form action="/zmaturuj.me/login" method="post">
-    <input type="text" name="email" placeholder="Zadajte vaše prihlasovacie meno" required class="input">
+<body>
 
-    <input type="password" name="password" placeholder="Zadajte heslo" required class="input">
+  <main class="website-content">
+    <?php $msg->display(); ?>
+    <h1>Prihlásenie</h1>
+    <form action="/zmaturuj.me/login" method="post">
+      <input type="text" name="email" placeholder="Zadajte vaše prihlasovacie meno" required class="input">
 
-    <button type="submit" name="submit" class="buttonInput">Prihlásenie</button>
+      <input type="password" name="password" placeholder="Zadajte heslo" required class="input">
 
-  </form>
-</main>
+      <button type="submit" name="submit" class="buttonInput">Prihlásenie</button>
 
-<?php
-// INCLUDE FOOTER
-include_once "parts/footer.php";
-?>
+    </form>
+  </main>
+
+  <?php
+  // INCLUDE FOOTER
+  include_once "parts/footer.php";
+  ?>
